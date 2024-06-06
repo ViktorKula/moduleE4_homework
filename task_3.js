1716b225d17180af26c639be780f04a99b012311
ViktorKula <vicyru@yandex.ru>
2024-06-06 21:43:19 +0300

    add task 3 update

--- task_3.js
+++ task_3.js
@@ -1,7 +1,13 @@
-const obj = function () {
-    Object.create(null);
+// const obj = function () {
+//     Object.create(null);
+// }
+//
+// console.log(obj)
+
+const obj = newObject();
+
+function newObject() {
+    return new Object();
 }
 
 console.log(obj)
-
-
